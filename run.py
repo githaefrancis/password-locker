69196fa330b4cbe64952aacd03fa521ad7088ac1
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-22 23:55:06 +0300

    add logic to register user

--- run.py
+++ run.py
@@ -19,3 +19,33 @@ def save_user(user):
 
 
 def main():
+  print('Welcome to Passord Locker?  \n')
+
+  print(f"What action would you like to take? ")
+  print('\n')
+
+  while True:
+    print("Use these short codes: new - create your account, login - login to your account")
+    short_code=input().lower()
+
+    if short_code=='new':
+      print("Create Account")
+      print("-"*10)
+      print("First name ...")
+      first_name=input()
+      print("Last Name ...")
+      last_name=input()
+
+      print("Email ...")
+      email=input()
+
+      print("username ...")
+      username=input()
+
+      print("Password ...")
+      password=input()
+      save_user(create_user(first_name,last_name,email,username,password))
+      print('\n')
+      print(f"Account created, Your username is {username}")
+if(__name__=='__main__'):
+  main()
