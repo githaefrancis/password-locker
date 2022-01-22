21351c0f6cd88127cf8cf966e23c6d6302523120
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-22 13:13:06 +0300

    create test class for user

---
+++ user_test.py
@@ -0,0 +1,14 @@
+import unittest  
+from user import User
+
+class TestUser(unittest.TestCase):
+
+  '''
+  Test class that defines test cases for the user class behaviours.
+  
+  Args:
+      unittest.TestCase:TestCase class that helps in creating test cases.
+  '''
+
+  
+
