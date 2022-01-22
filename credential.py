cb1a8198e9bcba402172f8ebd58cf250b6337827
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-22 18:07:58 +0300

    add init method for the credential class

---
+++ credential.py
@@ -0,0 +1,20 @@
+class Credential:
+  """
+  Class that generates new instances new instances of credential
+  """
+  pass
+
+  def __init__(self,platform_name,username,password):
+    '''
+    __init__ method that defines properties of the credential class
+
+    Args:
+        platform_name: Name of the platform the credential belongs to eg facebook, twitter
+        username: The login identifier for the account
+        password: The login password for the new account
+    '''
+
+    self.platform_name=platform_name
+    self.password=password
+
+
